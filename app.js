64d97a34ff7a96eb475a68f9d51b0024e1d32380
robgnyc <rg@mediumstudios.com>
2016-03-31 13:33:22 -0400

    Panelsldier

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ if(isHashPresent()) {
     		var data = response.data;
 
         console.log(data);
-        $.panelslider($('#my-link'));
+        panelslider($('#my-link'));
 
     		data.forEach(function(data) {
     			var url = data.images.thumbnail.url;
