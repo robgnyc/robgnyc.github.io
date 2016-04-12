3d2b5893bd91a0103240a53c2fa076e2db6fa3d7
robgnyc <rg@mediumstudios.com>
2016-04-12 06:38:52 -0400

    trying again

--- js/app.js
+++ js/app.js
@@ -91,10 +91,7 @@ if(isHashPresent()) {
 
          $("#section1").empty();
           
-         if (x == 0) {
-
-          $( "div#section1" ).replaceWith('<div id="section1" class="section oneslide"><h1>Add some slides</h1>');
-        }
+         
 
         var values = results.val();
         
@@ -107,7 +104,12 @@ if(isHashPresent()) {
 
         // have to take a different approach if only one image and make slide background of section rather than add a slide to section
 
-        if (x == 1) {
+        if (x == 0) {
+
+          $( "div#section1" ).replaceWith('<div id="section1" class="section oneslide"><h1>Add some slides</h1>');
+        }
+
+        else if (x == 1) {
             // console.log(numImages);
           $( "div#section1" ).replaceWith('<div class="section oneslide" id="section1" data-id="' + key + '"style="background-image: url(' + image.src + ')">');
           } 
